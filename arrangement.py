2f5978729ab807ab56afeb4a7837a28d23bd986a
Abrams <yuanchianchen@gmail.com>
2024-05-25 23:27:05 +0800

    Version 0.3.1.0 arrangement_over

---
+++ arrangement.py
@@ -0,0 +1,11 @@
+def arrangement(a,b):
+    total=a**b
+    c=[list() for _ in range(total)]
+    for i in range(total):
+        d=i
+        for j in range(b):
+            c[i].append(d%a)
+            d=d//a
+    return c
+
+print(arrangement(2,10))
