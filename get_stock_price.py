4761a02801005f44d706c385b0e08de1314f9c87
Abrams <yuanchianchen@gmail.com>
2024-05-26 18:49:40 +0800

    Version 0.4.1.1 get_stock_price_get_2330

--- get_stock_price.py
+++ get_stock_price.py
@@ -2,15 +2,18 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-import selenium
 import time
+
 #config
-PATH="D:/Stock AI/chromedriver-win64/chromedriver.exe"
-driver = webdriver.Chrome(PATH)
+driver = webdriver.Chrome()
 driver.get("https://www.twse.com.tw/zh/trading/historical/stock-day.html")
+
 #search
-stock_no=driver.find_element(By.NAME,"stockNo")
+stock_no = driver.find_element(By.NAME, "stockNo")
 stock_no.send_keys("2330")
 stock_no.send_keys(Keys.RETURN)
+
+search=driver.find_elements(By.CLASS_NAME, "search")
+search[2].click()
 
 time.sleep(3)
