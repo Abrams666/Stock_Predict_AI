7eeec4d79346b3fd24beb64b5ffd9b59afd21381
Abrams <yuanchianchen@gmail.com>
2024-05-26 17:11:31 +0800

    Version 0.4.0.0 get_stock_price_start

--- get_stock_price.py
+++ get_stock_price.py
@@ -1,6 +1,16 @@
 #imports
-import cross_multiplication
-import array_plus 
-import arrangement
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import selenium
+import time
+#config
+PATH="D:/Stock AI/chromedriver-win64/chromedriver.exe"
+driver = webdriver.Chrome(PATH)
+driver.get("https://www.twse.com.tw/zh/trading/historical/stock-day.html")
+#search
+stock_no=driver.find_element(By.NAME,"stockNo")
+stock_no.send_keys("2330")
+stock_no.send_keys(Keys.RETURN)
 
-print(arrangement.arrangement(3,2))
+time.sleep(3)
