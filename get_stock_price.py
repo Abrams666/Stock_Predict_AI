fca3dc2d9749627a65a07985af1db5928bdd5c15
Abrams <yuanchianchen@gmail.com>
2024-05-26 19:35:01 +0800

    Version 0.4.2.0 get_stock_price_over

--- get_stock_price.py
+++ get_stock_price.py
@@ -2,6 +2,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import Select
 import time
 
 #config
@@ -11,9 +12,27 @@ driver.get("https://www.twse.com.tw/zh/trading/historical/stock-day.html")
 #search
 stock_no = driver.find_element(By.NAME, "stockNo")
 stock_no.send_keys("2330")
-stock_no.send_keys(Keys.RETURN)
 
-search=driver.find_elements(By.CLASS_NAME, "search")
-search[2].click()
+for i in range(99,114):
+    for j in range(1,13):
+        year_select=driver.find_element(By.NAME,"yy")
+        year_option=Select(year_select)
+        year_option.select_by_visible_text("民國 "+ str(i) +" 年")
+        
+        month_select=driver.find_element(By.NAME,"mm")
+        month_option=Select(month_select)
+        if (len(str(j))==1):
+            month_option.select_by_visible_text("0"+str(j) +"月")
+        elif(len(str(j))==2):
+            month_option.select_by_visible_text(str(j) +"月")
 
-time.sleep(3)
+        search=driver.find_elements(By.CLASS_NAME, "search")
+        search[2].click()
+
+        time.sleep(1)
+
+        download=driver.find_element(By.CLASS_NAME,"csv")
+        download.click()
+        print("Data "+str(i)+"/"+str(j)+" downloaded")
+
+        time.sleep(2)
