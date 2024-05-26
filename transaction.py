acd7928fb4378016eeef975f08331406984c8067
Abrams <yuanchianchen@gmail.com>
2024-05-26 23:07:54 +0800

    Version 0.6.2.1 transaction_transaction_over

--- transaction.py
+++ transaction.py
@@ -27,6 +27,66 @@ def transaction_val(buy_weight,buy_quantity_weight,day):
 
     return value
 
-buy=[1,1,1]
-buy_quantity=[1,1,1]
-print(transaction_val(buy,buy_quantity,3))
+def transaction(buy_weight,buy_quantity_weight,keep_weight,keep_quantity_weight,sell_weight,sell_quantity_weight):
+    #imports
+    import openpyxl
+
+    #read excel price and quantity
+    file_path = "D:/Stock AI/Stock_Price_Data/Stock_tsmc.xlsx"
+    workbook = openpyxl.load_workbook(file_path)
+    sheet = workbook.active
+
+    #ZeroMoney
+    money=0
+    stock=0
+
+    #find start day
+    max_day=len(buy_weight)
+    if(max_day<len(buy_quantity_weight)):
+        max_day=len(buy_quantity_weight)
+    if(max_day<len(keep_weight)):
+        max_day=len(keep_weight)
+    if(max_day<len(keep_quantity_weight)):
+        max_day=len(keep_quantity_weight)
+    if(max_day<len(sell_weight)):
+        max_day=len(sell_weight)
+    if(max_day<len(sell_quantity_weight)):
+        max_day=len(sell_quantity_weight)
+
+    #get three value
+    for i in range(max_day,31): #2533
+        buy_value=transaction_val(buy_weight,buy_quantity_weight,i)
+        keep_value=transaction_val(keep_weight,keep_quantity_weight,i)
+        sell_value=transaction_val(sell_weight,sell_quantity_weight,i)
+
+        #choose
+        if(buy_value>keep_value and buy_value>sell_value):
+            buy_num=int(buy_value)
+            stock=stock+buy_num
+            money=money-(buy_num*float(sheet["D"+str(i+1)].value)*1000)
+            print("Day"+str(i)+" Buy"+str(buy_num))
+        
+        elif(keep_value>buy_value and keep_value>sell_value):
+            print("Day"+str(i)+" Keep")
+
+        elif(sell_value>buy_value and sell_value>keep_value):
+            sell_num=int(sell_value)
+            if((stock-sell_num)>=0):
+                stock=stock-sell_num
+                money=money+(sell_num*float(sheet["D"+str(i+1)].value)*1000)
+            else:
+                money=money+(stock*float(sheet["D"+str(i+1)].value)*1000)
+                stock=0
+            print("Day"+str(i)+" Sell"+str(sell_num))
+        print(" Money:"+str(money))
+    #end count
+    money=money+(stock*867000)
+    return money/stock
+
+buy_weight=[1,2,3]
+buy_quantity_weight=[1,2,3]
+keep_weight=[2,2,2]
+keep_quantity_weight=[2,2,2]
+sell_weight=[3,2,1]
+sell_quantity_weight=[3,2,1]
+print(transaction(buy_weight,buy_quantity_weight,keep_weight,keep_quantity_weight,sell_weight,sell_quantity_weight))
