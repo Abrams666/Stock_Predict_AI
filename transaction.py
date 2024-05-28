53ca6a22fa16bc47216a5efd41ed3b3352d82c03
Abrams <yuanchianchen@gmail.com>
2024-05-28 23:08:45 +0800

    Version 0.6.2.3 transaction_efficiency_and_logic_update

--- transaction.py
+++ transaction.py
@@ -1,41 +1,22 @@
-def transaction_val(buy_weight,buy_quantity_weight,day):
+def transaction_val(buy_weight,buy_quantity_weight,stock_price,stock_quantity,day):
     #imports
     import cross_multiplication as cm
     import array_plus as ap
-    import openpyxl
-
-    #read excel price and quantity
-    file_path = "D:/Stock AI/Stock_Price_Data/Stock_tsmc.xlsx"
-    workbook = openpyxl.load_workbook(file_path)
-    sheet = workbook.active
-    price_row_data = []
-    quantity_row_data = []
-    for i in range(2, 3533):
-        price_row_data.append(float(sheet["D"+str(i)].value))
-        quantity_row_data.append(float(sheet["G"+str(i)].value))
 
     price_data=[]
-    for i in range(day-len(buy_weight),day):
-        price_data.append(float(price_row_data[i]))
+    for i in range(day-len(buy_weight)+1,day+1):
+        price_data.append(float(stock_price[i-1]))
 
     quantity_data=[]
-    for i in range(day-len(buy_quantity_weight),day):
-        quantity_data.append(float(quantity_row_data[i]))
+    for i in range(day-len(buy_quantity_weight)+1,day+1):
+        quantity_data.append(float(stock_quantity[i-1]))
     
     #count
     value=ap.array_plus(cm.cross_multiplication(price_data,buy_weight))+ap.array_plus(cm.cross_multiplication(quantity_data,buy_quantity_weight))
 
     return value
 
-def transaction(buy_weight,buy_quantity_weight,keep_weight,keep_quantity_weight,sell_weight,sell_quantity_weight):
-    #imports
-    import openpyxl
-
-    #read excel price and quantity
-    file_path = "D:/Stock AI/Stock_Price_Data/Stock_tsmc.xlsx"
-    workbook = openpyxl.load_workbook(file_path)
-    sheet = workbook.active
-
+def transaction(buy_weight,buy_quantity_weight,keep_weight,keep_quantity_weight,stock_price,stock_quantity):
     #ZeroMoney
     money=1000
     stock=0
@@ -48,44 +29,48 @@ def transaction(buy_weight,buy_quantity_weight,keep_weight,keep_quantity_weight,
         max_day=len(keep_weight)
     if(max_day<len(keep_quantity_weight)):
         max_day=len(keep_quantity_weight)
-    if(max_day<len(sell_weight)):
-        max_day=len(sell_weight)
-    if(max_day<len(sell_quantity_weight)):
-        max_day=len(sell_quantity_weight)
 
     #get three value
-    for i in range(2500,2533): #max_day,2533
-        buy_value=transaction_val(buy_weight,buy_quantity_weight,i)
-        keep_value=transaction_val(keep_weight,keep_quantity_weight,i)
-        sell_value=transaction_val(sell_weight,sell_quantity_weight,i)
+    for i in range(2500,len(stock_price)+1): #max_day
+        buy_value=transaction_val(buy_weight,buy_quantity_weight,stock_price,stock_quantity,i)
+        keep_value=transaction_val(keep_weight,keep_quantity_weight,stock_price,stock_quantity,i)
 
         #choose
-        if(buy_value>keep_value and buy_value>sell_value):
-            buy_num=int(buy_value)
-            if(money-(buy_num*float(sheet["D"+str(i+1)].value))<0):
-                buy_num=money//float(sheet["D"+str(i+1)].value)
-            stock=stock+buy_num
-            money=money-(buy_num*float(sheet["D"+str(i+1)].value))
-            print("Day"+str(i)+" Buy:"+str(buy_num)+" Stock:"+str(stock)+" Stock Price:"+str(sheet["D"+str(i+1)].value)+" Money:"+str(int(money)))
+        x=buy_value
+        if(x<0):
+            x=-x
         
-        elif(keep_value>buy_value and keep_value>sell_value):
-            print("Day"+str(i)+" Keep"+" Stock:"+str(stock)+" Stock Price:"+str(sheet["D"+str(i+1)].value)+" Money:"+str(int(money)))
+        if(x>keep_value):
+            buy_num=int(buy_value)
+            if(money-buy_num*stock_price[i-1]<0):
+                buy_num=money//stock_price[i-1]
+            if(stock+buy_num<0):
+                buy_num=-stock
+            stock=stock+buy_num
+            money=money-buy_num*stock_price[i-1]
+            print("Day"+str(i)+" Num:"+str(buy_num)+" Stock:"+str(stock)+" Stock Price:"+str(stock_price[i-1])+" Money:"+str(int(money)))
+        elif(keep_value>=buy_value):
+            print("Day"+str(i)+" Num:0 Stock:"+str(stock)+" Stock Price:"+str(stock_price[i-1])+" Money:"+str(int(money)))
 
-        elif(sell_value>buy_value and sell_value>keep_value):
-            sell_num=int(sell_value)
-            if((stock-sell_num)<0):
-                sell_num=stock
-            money=money+(stock*float(sheet["D"+str(i+1)].value))
-            stock=stock-sell_num
-            print("Day"+str(i)+" Sell:"+str(sell_num)+" Stock:"+str(stock)+" Stock Price:"+str(sheet["D"+str(i+1)].value)+" Money:"+str(int(money)))
     #end count
-    money=money+stock*float(sheet["D"+str(31)].value)
+    money=money+stock*float(len(stock_price)-1)
     return money
 
-buy_weight=[1,2,3]
-buy_quantity_weight=[1,2,3]
-keep_weight=[2,2,2]
-keep_quantity_weight=[2,2,2]
-sell_weight=[3,2,1]
-sell_quantity_weight=[3,2,1]
-print(transaction(buy_weight,buy_quantity_weight,keep_weight,keep_quantity_weight,sell_weight,sell_quantity_weight))
+import openpyxl
+
+file_path = "D:/Stock AI/Stock_Price_Data/Stock_tsmc.xlsx"
+workbook = openpyxl.load_workbook(file_path)
+sheetx = workbook.active
+
+stock_price=[]
+stock_quantity=[]
+for i in range(2,3534):
+    stock_price.append(float(sheetx["D"+str(i)].value))
+    stock_quantity.append(float(sheetx["G"+str(i)].value))
+
+buy_weight=[-1,0,1]
+buy_quantity_weight=[-1,0,1]
+keep_weight=[0.5,0,-0.5]
+keep_quantity_weight=[0.5,0,-0.5]
+
+print(transaction(buy_weight,buy_quantity_weight,keep_weight,keep_quantity_weight,stock_price,stock_quantity))
