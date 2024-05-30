1078e67022f7572ab98fb8a9724213871272ce96
Abrams <yuanchianchen@gmail.com>
2024-05-30 22:59:01 +0800

    Version 0.7.1.4

--- train.py
+++ train.py
@@ -4,7 +4,7 @@ import arrangement as ar
 import openpyxl
 
 #load excel data
-file_path = "D:/Stock AI/Stock_Price_Data/Stock_00940.xlsx"
+file_path = "D:/Stock AI/Stock_Price_Data/Stock_tsmc.xlsx"
 workbook = openpyxl.load_workbook(file_path)
 sheetx = workbook.active
 
@@ -18,7 +18,7 @@ for i in range(2,42):
 def correct_weight(weights):
     for i in range(len(weights)):
         for j in range(len(weights[i])):
-            weights[i][j]=(weights[i][j]-100)/1
+            weights[i][j]=(weights[i][j]-1)/10
     
     return weights
 
@@ -26,20 +26,25 @@ def correct_weight(weights):
 print("start")
 money=0
 max_money=0
-best=[]
+best=[0,0,0,0]
+x=0
 
-day_arr=ar.arrangement(len(stock_price),4)
+day_arr=ar.arrangement(1,4) #len(stock_price)
 for i in range(len(day_arr)):
     for j in range(4):
-        day_arr[i][j]=day_arr[i][j]+1
+        day_arr[i][j]=day_arr[i][j]+3
 print("Days_Arrangment Done")
 print(day_arr)
 
 for i in range(len(day_arr)):
-    price_weights=ar.arrangement(201,day_arr[i][0])
-    price_quantity_weights=ar.arrangement(201,day_arr[i][1])
-    keep_weights=ar.arrangement(201,day_arr[i][2])
-    keep_quantity_weights=ar.arrangement(201,day_arr[i][3])
+    price_weights=ar.arrangement(3,day_arr[i][0])
+    print("price_weights")
+    price_quantity_weights=ar.arrangement(3,day_arr[i][1])
+    print("price_quantity_weights")
+    keep_weights=ar.arrangement(3,day_arr[i][2])
+    print("keep_weights")
+    keep_quantity_weights=ar.arrangement(3,day_arr[i][3])
+    print("keep_quantity_weights")
 
     price_weights=correct_weight(price_weights)
     price_quantity_weights=correct_weight(price_quantity_weights)
@@ -47,20 +52,24 @@ for i in range(len(day_arr)):
     keep_quantity_weights=correct_weight(keep_quantity_weights)
 
     print("Weights_Arrangment "+str(i)+" Done")
-    print(price_weights)
 
     for j in range(0,len(price_weights)):
+        print("j")
         for k in range(0,len(price_quantity_weights)):
             for m in range(0,len(keep_weights)):
                 for n in range(0,len(keep_quantity_weights)):
                     money=ts.transaction(price_weights[j],price_quantity_weights[k],keep_weights[m],keep_quantity_weights[n],stock_price,stock_quantity)
                     if (money>max_money):
                         max_money=money
-                        best.append(i)
-                        best.append(j)
-                        best.append(k)
-                        best.append(m)
-                        best.append(n)
-                        print(best)
+                        best[0]=j
+                        best[1]=k
+                        best[2]=m
+                        best[3]=n
+                    x=x+1
+                    print(x)
 
-print(best)
+print(price_weights[best[0]])
+print(price_quantity_weights[best[1]])
+print(keep_weights[best[2]])
+print(keep_quantity_weights[best[3]])
+print(max_money)
