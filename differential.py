a45275433458fbb8a85163ec68e53f79527182ad
Abrams <yuanchianchen@gmail.com>
2024-08-04 09:44:16 +0800

    Version 1.4.0 standardize

--- differential.py
+++ differential.py
@@ -12,14 +12,14 @@ def differential(best_situation,price_weight,quantity_weight,stock_price,stock_q
     m=0
     if(j<len(price_weight)):
         for i in range(max_day,len(best_situation)+1):
-            m=m+stock_price[i+j-max_day]*(best_situation[i-1]-ts.transaction_val(price_weight,quantity_weight,stock_price,stock_quantity,b,i))
+            m=m+stock_price[i+j-max_day]*(ts.transaction_val(price_weight,quantity_weight,stock_price,stock_quantity,b,i)-best_situation[i-1])
         m=m/(len(best_situation)+1-max_day)
     elif(j<len(price_weight)+len(quantity_weight)):
         for i in range(max_day,len(best_situation)+1):
-            m=m+stock_quantity[i+j-max_day-max_day]*(best_situation[i-1]-ts.transaction_val(price_weight,quantity_weight,stock_price,stock_quantity,b,i))
+            m=m+stock_quantity[i+j-max_day-max_day]*(ts.transaction_val(price_weight,quantity_weight,stock_price,stock_quantity,b,i)-best_situation[i-1])
         m=m/(len(best_situation)+1-max_day)
     else:
         for i in range(max_day,len(best_situation)+1):
-            m=m+(best_situation[i-1]-ts.transaction_val(price_weight,quantity_weight,stock_price,stock_quantity,b,i))
+            m=m+(ts.transaction_val(price_weight,quantity_weight,stock_price,stock_quantity,b,i)-best_situation[i-1])
         m=m/(len(best_situation)+1-max_day)
     return m
--- train.py
+++ train.py
@@ -33,65 +33,68 @@ for i in range(2,len(sheetx["A"])+1):
 day_arr=[[6,6]]
 
 #gradient decent
-learn_rate=0.0000000001
+learn_rate=0.0000000000001
 max_money=0
 money=0
-best_price_weight=[0,0,0,0,0,0]
-best_quantity_weight=[0,0,0,0,0,0]
+best_price_weight=[]
+best_quantity_weight=[]
 best_b=-100
 
+#all day arrangement
 for i in range(len(day_arr)):
     #init weight
     price_weight=[]
     quantity_weight=[]
     b=-100
-    new_price_weight=[0,0,0,0,0,0]
-    new_quantity_weight=[0,0,0,0,0,0]
+    new_price_weight=[]
+    new_quantity_weight=[]
     new_b=-100
 
     for j in range(day_arr[i][0]):
-        price_weight.append(-0.01)
-        new_price_weight.append(-0.01)
+        price_weight.append(0.01)
+        new_price_weight.append(0.01)
 
     for j in range(day_arr[i][1]):
-        quantity_weight.append(-0.01)
-        new_quantity_weight.append(-0.01)
+        quantity_weight.append(0.01)
+        new_quantity_weight.append(0.01)
 
     x=1
     ms= [0] * (len(price_weight) + len(quantity_weight) + 1)
     m_is_0=0
-    while(m_is_0==0 and x<101):
+
+    #start train
+    while(m_is_0==0):
         m_is_0=1
 
+        #change new weights
         for j in range(day_arr[i][0]+day_arr[i][1]+1):
             m=df.differential(best_situation,price_weight,quantity_weight,stock_price,stock_quantity,b,j)
             ms[j]=m
             if (m<0.1 and m>-0.1):
-                continue
+                m_is_0=1
             else:
                 if(j<len(price_weight)):
-                    new_price_weight[j]=price_weight[j]+learn_rate*(m)
-                    m_is_0=0
+                    new_price_weight[j]=price_weight[j]-learn_rate*(m)
                 elif(j<len(price_weight)+len(quantity_weight)):
-                    new_quantity_weight[j-day_arr[i][0]]=quantity_weight[j-day_arr[i][0]]+learn_rate*(m)
-                    m_is_0=0
+                    new_quantity_weight[j-day_arr[i][0]]=quantity_weight[j-day_arr[i][0]]-learn_rate*(m)
                 else:
-                    new_b=b+learn_rate*(m)
-                    m_is_0=0
+                    new_b=b-learn_rate*(m)
 
-        # for j in range(len(price_weight)):
-        #     price_weight[j]=new_price_weight[j]
-        # for j in range(len(quantity_weight)):
-        #     quantity_weight[j]=new_quantity_weight[j]
+                m_is_0=0
+
+        #update weights
         price_weight=new_price_weight[:]
         quantity_weight=new_quantity_weight[:]
         b=new_b
 
+        #break while enough accuracy
         if(m_is_0==1):
             break
 
+        #count money
         money=ts.transaction(price_weight,quantity_weight,stock_price,stock_quantity,b)
 
+        #print current situation
         print("D"+str(x))
         print(ms)
         print(price_weight)
@@ -99,13 +102,10 @@ for i in range(len(day_arr)):
         print(b)
         print(money)
 
-        if(money>max_money and x>10):
+        #update best weights while max money is higher
+        if(money>max_money and x>0):
             print("********************************************")
             max_money=money
-            # for j in range(len(price_weight)):
-            #     best_price_weight[j]=price_weight[j]
-            # for j in range(len(quantity_weight)):
-            #     best_quantity_weight[j]=quantity_weight[j]
             best_price_weight=price_weight[:]
             best_quantity_weight=quantity_weight[:]
             best_b=b
@@ -113,7 +113,7 @@ for i in range(len(day_arr)):
 
         x=x+1
 
-#result
+#print result
 print("--------------------------------------")
 print(best_price_weight)
 print(best_quantity_weight)
